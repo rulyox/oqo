9b2fe20fa9fd3887cf1b54db7ab08b8ab46f318b
rulyox <email@rulyox.com>
2020-08-07 14:06:56 +0900

    Added where clause

--- lib/oqo.ts
+++ lib/oqo.ts
@@ -1,6 +1,6 @@
 export class OQO {
 
-    private selectList: String[];
+    private selectList: string[];
     private objectList: any[];
 
     constructor() {
@@ -8,7 +8,7 @@ export class OQO {
         this.objectList = [];
     }
 
-    select(selectList: String[]) {
+    select(selectList: string[]) {
 
         this.selectList = selectList;
 
@@ -20,6 +20,7 @@ export class OQO {
 
         if(Array.isArray(object)) {
 
+            // check if object array
             object.forEach((item) => {
 
                 if(typeof item !== 'object') throw new Error('FROM clause needs an object or an array of objects!');
@@ -30,6 +31,7 @@ export class OQO {
 
         } else {
 
+            // check if single object
             if(typeof object === 'object') this.objectList = [object];
             else throw new Error('FROM clause needs an object or an array of objects!');
 
@@ -39,14 +41,84 @@ export class OQO {
 
     }
 
-    run() {
+    where(condition: string) {
 
+        // parse condition
+        const splitCondition = condition.split(' ');
+
+        if(splitCondition.length !== 3) throw new Error('WHERE clause needs to have two spaces!');
+
+        const column = splitCondition[0];
+        const operator = splitCondition[1];
+        const operand = splitCondition[2];
+
+        if(operator !== '>' && operator !== '>=' && operator !== '=' && operator !== '<' && operator !== '<=') throw new Error('WHERE clause needs to have the correct operator!');
+
+        // create conditional statement
+        let statement: any;
+
+        switch(operator) {
+            case '>':
+                statement = (item: any): boolean => isNaN(Number(item[`${column}`])) ?
+                    item[`${column}`] > operand :
+                    item[`${column}`] > Number(operand);
+
+                break;
+
+            case '>=':
+                statement = (item: any): boolean => isNaN(Number(item[`${column}`])) ?
+                    item[`${column}`] >= operand :
+                    item[`${column}`] >= Number(operand);
+
+                break;
+
+            case '=':
+                statement = (item: any) => isNaN(Number(item[`${column}`])) ?
+                    item[`${column}`] === operand :
+                    item[`${column}`] === Number(operand);
+
+                break;
+
+            case '<':
+                statement = (item: any) => isNaN(Number(item[`${column}`])) ?
+                    item[`${column}`] < operand :
+                    item[`${column}`] < Number(operand);
+
+                break;
+
+            case '<=':
+                statement = (item: any) => isNaN(Number(item[`${column}`])) ?
+                    item[`${column}`] <= operand :
+                    item[`${column}`] <= Number(operand);
+
+                break;
+        }
+
+        // filter objects
         const objectList: any[] = [];
 
         this.objectList.forEach((item) => {
 
+            if(statement(item)) objectList.push(item);
+
+        });
+
+        this.objectList = objectList;
+
+        return this;
+
+    }
+
+    run() {
+
+        const objectList: any[] = [];
+
+        // for all objects
+        this.objectList.forEach((item) => {
+
             const object: any = {};
 
+            // filter selected columns
             this.selectList.forEach((column) => {
 
                 object[`${column}`] = item[`${column}`];
@@ -57,7 +129,9 @@ export class OQO {
 
         });
 
-        return objectList;
+        this.objectList = objectList;
+
+        return this.objectList;
 
     }
 
--- test.js
+++ test.js
@@ -1,14 +1,17 @@
 const OQO = require('./dist');
 
-let people = [
+const people = [
     { name: 'John', age: 25, gender: 'male' },
     { name: 'Alice', age: 14, gender: 'female' },
-    { name: 'Bob', age: 19, gender: 'male' }
+    { name: 'Bob', age: 19, gender: 'male' },
+    { name: 'Charlie', age: 20, gender: 'female' },
+    { name: 'David', age: 21, gender: 'male' }
 ];
 
-const query = OQO
+const result = OQO
     .select(['name', 'gender'])
     .from(people)
+    .where('age >= 20')
     .run();
 
-console.log(query);
+console.log(result);
